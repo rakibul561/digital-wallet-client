88d548204a19414464a16504eb6f2018d68040f7
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-08-28 07:34:23 -0700

    withdraw confram

--- src/pages/user/Withdraw.tsx
+++ src/pages/user/Withdraw.tsx
@@ -1,23 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { useUserInfoQuery } from "@/redux/features/auth/auth.api";
 import { useWalletQuery } from "@/redux/features/wallets/wallet.api";
-import { data } from "react-router";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type WithdrawFormValues = {
+  amount: string;
+};
 
 const Withdraw = () => {
   const { data: userInfo } = useUserInfoQuery(undefined);
   const walletId = userInfo?.data?.walletId;
   console.log(walletId);
-  const {
-    data: walletData,
-    isLoading,
-    isError,
-  } = useWalletQuery(walletId!, {
+
+  const { data: walletData, isLoading, isError } = useWalletQuery(walletId!, {
     skip: !walletId,
   });
 
- const wallet = walletData?.data;
+  const form = useForm<WithdrawFormValues>({
+    defaultValues: {
+      amount: "",
+    },
+  });
 
+  const onSubmit: SubmitHandler<WithdrawFormValues> = (data) => {
+    console.log("Withdraw data:", data);
+  };
+   
+
+   const wallet = walletData?.data
   return (
     <div>
+
       <div className="bg-gradient-to-r from-indigo-500 to-purple-500 text-white p-6 rounded-xl shadow-md flex justify-between items-center">
         <div>
           <h3 className="text-lg font-semibold">Digital Wallet</h3>
@@ -30,6 +52,29 @@ const Withdraw = () => {
           ACTIVE
         </span>
       </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="Enter amount" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full">
+            Withdraw
+          </Button>
+        </form>
+      </Form>
+
+
     </div>
   );
 };
